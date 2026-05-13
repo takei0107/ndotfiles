fd434b451547a8ad4ab2809f3b25f4b9d8a252a6
takei0107 <takei0107.work@gmail.com>
2026-05-13 22:48:21 +0900

    fix

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -169,8 +169,6 @@ local function lsp_settings()
         workspace = {
           library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
             "${3rd}/luv/library",
-            "${3rd}/busted/library",
-            "${3rd}/luassert/library",
           }),
           checkThirdParty = "Disable",
         },
